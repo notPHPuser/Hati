35f91b97961c97710c5a1e01952a90cf4f762588
notPHPuser <artem2005dm@mail.ru>
2024-03-06 21:33:27 +0300

    hati
    change react-router

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,49 +1,19 @@
-import React from 'react';
 import './app.css';
-import { createBrowserRouter, RouterProvider, Route, Link } from 'react-router-dom';
-import Arenda from './pages/arenda/Arenda';
-import SuperMainPage from './SuperMainPage';
-import Sell from './pages/sell/Sell';
-import LoginOrReg from './components/log-res/LoginOrReg';
-import OfrtenFlat from './pages/OftenList/buyFlat/OfrtenFlat';
-import BuyRoom from './pages/OftenList/buyRoom/BuyRoom';
-import ListReg from './components/log-res/ListReg';
-
-const router = createBrowserRouter([
-  {
-    path: '/arenda',
-    element: <Arenda />,
-  },
-  {
-    path: '/',
-    element: <SuperMainPage />,
-  },
-  {
-    path: '/sell',
-    element: <Sell />,
-  },
-  {
-    path: '/log-res',
-    element: <LoginOrReg />,
-  },
-  {
-    path: '/buyFlat',
-    element: <OfrtenFlat />,
-  },
-  {
-    path: '/buyRoom',
-    element: <BuyRoom />,
-  },
-  {
-    path: '/listReg',
-    element: <ListReg />,
-  },
-]);
+import React from 'react';
+import Header from './components/header/Header';
+import Main from './components/main/Main';
+import Discount from './components/discount/Discount';
+import OftenFind from './components/oftenfind/OftenFind';
+import ListOfFlats from './components/mayBeGood/listOfFlats';
 
 const App = () => {
   return (
     <>
-      <RouterProvider router={router} />
+      <Header />
+      <Main />
+      <Discount />
+      <OftenFind />
+      <ListOfFlats />
     </>
   );
 };
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.jsx';
 import './index.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Arenda from './pages/arenda/Arenda';
+import Sell from './pages/sell/Sell';
+import LoginOrReg from './components/log-res/LoginOrReg';
+import OfrtenFlat from './pages/OftenList/buyFlat/OfrtenFlat';
+import BuyRoom from './pages/OftenList/buyRoom/BuyRoom';
+import ListReg from './components/log-res/ListReg';
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/arenda',
+    element: <Arenda />,
+  },
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/sell',
+    element: <Sell />,
+  },
+  {
+    path: '/log-res',
+    element: <LoginOrReg />,
+  },
+  {
+    path: '/buyFlat',
+    element: <OfrtenFlat />,
+  },
+  {
+    path: '/buyRoom',
+    element: <BuyRoom />,
+  },
+  {
+    path: '/listReg',
+    element: <ListReg />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
